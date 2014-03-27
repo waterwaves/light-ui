ee9bac08cb672027aaf4f1a558df6590cce0e347
Shan He <sean.shanhe@gmail.com>
2014-03-27 00:10:05 -0400

    add side nav bar (left), and mobile grid system

--- css/demo.js
+++ css/demo.js
@@ -1,6 +1,8 @@
 $(function() {
-  var $bottomNavBar = $('[data-type=nav].bottom.fixed');
-  var $topNavBar = $('[data-type=nav].top.fixed');
+  // Adjust the main-container margin-top or margin-bottom based on the
+  // height of navbar.
+  var $bottomNavBar = $('[data-type=navigation].bottom.fixed');
+  var $topNavBar = $('[data-type=navigation].top.fixed');
   if ($bottomNavBar || $topNavBar) {
     var $mainContainer = $('.main-container');
     var mainContainerMarginTopBottom = $mainContainer.outerHeight(true) - $mainContainer.outerHeight();
@@ -12,5 +14,21 @@ $(function() {
       var navHeight = $topNavBar.height();
       $mainContainer.css('margin-top', mainContainerMarginTopBottom/2 + navHeight);
     }
+  }
+
+  // fix the side-nav-bar if it is required, based on scroll event.
+  var $sideNavBar = $('[data-type=navigation].left.fixed, [data-type=navigation].right.fixed');
+  if ($sideNavBar.length > 0) {
+    var $navContainer = $sideNavBar.children('.nav-container');
+    var navContainerWidth = $navContainer.width();
+    var toTop = $sideNavBar.data('top');
+    $(window).scroll(function() {
+      var offsetToWindow = $sideNavBar.offset().top - $(this).scrollTop();
+      if (offsetToWindow <= toTop) {
+        $navContainer.addClass('sticky').css('width', navContainerWidth).css('top', toTop);
+      } else {
+        $navContainer.removeClass('sticky').css('width', '').css('top', '');
+      }
+    });
   }
 });
